c307282b135e1fb40fa6ea3e61f7bfc862bcbf8f
nader2356 <naderabdessaied8@gmail.com>
2023-12-13 20:36:13 +0100

    23: chapter video

--- app/(dashboard)/(routes)/teachers/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teachers/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -2,13 +2,12 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
-
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
-
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterVideoForm } from "./_components/chapter-video-form";
 
 const ChapterIdPage = async ({
   params
@@ -103,6 +102,11 @@ const ChapterIdPage = async ({
               Add a video
             </h2>
           </div>
+          <ChapterVideoForm
+            initialData={chapter}
+            chapterId={params.chapterId}
+            courseId={params.courseId}
+          />
         </div>
       </div>
     </div>
