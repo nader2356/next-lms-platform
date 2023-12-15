d171fa27824ee928edd6db24271c5d790a6af6c9
nader2356 <naderabdessaied8@gmail.com>
2023-12-15 19:37:16 +0100

    27: categories

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,22 @@
+
+import { db } from "@/lib/db";
+
+import { Categories } from "./_components/categories";
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc"
+    }
+  });
+
+  return ( 
+    <div className="p-6">
+      <Categories
+        items={categories}
+      />
+    </div>
+   );
+}
+ 
+export default SearchPage;
