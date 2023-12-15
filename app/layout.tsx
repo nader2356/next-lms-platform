626d8b7fde738931bc372148f0e1cc940a2b81b2
nader2356 <naderabdessaied8@gmail.com>
2023-12-15 19:29:02 +0100

    25: course publish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ToastProvider } from '@/components/providers/toaster-provider'
+import { ConfettiProvider } from '@/components/providers/confetti-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ConfettiProvider />
           <ToastProvider />
           {children}
         </body>
