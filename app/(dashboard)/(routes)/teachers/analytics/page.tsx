28e08a231f83868c380c256a2222f8ab0ea39d73
nader2356 <naderabdessaied8@gmail.com>
2023-12-16 18:50:16 +0100

    36: analytics

--- app/(dashboard)/(routes)/teachers/analytics/page.tsx
+++ app/(dashboard)/(routes)/teachers/analytics/page.tsx
@@ -1,9 +1,44 @@
-const AnalyticsPage = () => {
+
+  import { auth } from "@clerk/nextjs";
+  import { redirect } from "next/navigation";
+  
+
+  
+  import { DataCard } from "./_components/data-card";
+  import { Chart } from "./_components/chart";
+import { getAnalytics } from "@/actions/ger-analytics";
+  
+  const AnalyticsPage = async () => {
+    const { userId } = auth();
+  
+    if (!userId) {
+      return redirect("/");
+    }
+  
+    const {
+      data,
+      totalRevenue,
+      totalSales,
+    } = await getAnalytics(userId);
+  
     return ( 
-      <div>
-        Analytics page
+      <div className="p-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+          <DataCard
+            label="Total Revenue"
+            value={totalRevenue}
+            shouldFormat
+          />
+          <DataCard
+            label="Total Sales"
+            value={totalSales}
+          />
+        </div>
+        <Chart
+          data={data}
+        />
       </div>
      );
   }
-  
+
   export default AnalyticsPage;
