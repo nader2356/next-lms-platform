81966144adbe3884e5f7aeb2b45032a2d70ad719
nader2356 <naderabdessaied8@gmail.com>
2023-12-16 17:39:56 +0100

    30: chapter layout

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -3,16 +3,14 @@ import { UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
-
 import { Button } from "@/components/ui/button";
-
 import { SearchInput } from "./search-input";
-
 export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isCoursePage = pathname?.includes("/courses");
   const isSearchPage = pathname === "/search";
 
   return (
@@ -23,7 +21,7 @@ export const NavbarRoutes = () => {
         </div>
       )}
       <div className="flex gap-x-2 ml-auto">
-        {isTeacherPage || isPlayerPage ? (
+        {isTeacherPage || isCoursePage ? (
           <Link href="/">
             <Button size="sm" variant="ghost">
               <LogOut className="h-4 w-4 mr-2" />
